c4acefb5889a2a80889e8209108826f6bdb367d8
Rashindra Yadav <yadavrashindra5@gmail.com>
2023-08-13 23:22:01 +0530

    find duplicate number

---
+++ src/main/java/org/example/coding/array/DuplicateNumber.java
@@ -0,0 +1,18 @@
+package org.example.coding.array;
+
+public class DuplicateNumber {
+    public void findDuplicate(int[] nums) {
+        // HashMap<Integer,Integer>map=new HashMap<>();
+        // for(int i=0;i<nums.length;++i){
+        //     int repeatCount=map.getOrDefault(nums[i],0);
+        //     if(repeatCount==1){
+        //         return nums[i];
+        //     }
+        //     else{
+        //         map.put(nums[i],1);
+        //     }
+        // }
+        // return -1;
+        System.out.println(Math.abs(-3));
+    }
+}
