25f4b29ce2a7a1eca1caae92e564fd2692aeb081
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-01-09 21:57:57 +0530

    jan-9

--- src/main/java/org/example/coding/array/ArrayProblem.java
+++ src/main/java/org/example/coding/array/ArrayProblem.java
@@ -142,4 +142,23 @@ public class ArrayProblem {
             j++;
         }
     }
+
+    /*
+    *
+    * Missing Number
+    *
+    *
+    *Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+    *
+    *
+    * */
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum+=nums[i];
+        }
+        int sum1=nums.length*(nums.length+1)/2;
+        return sum1-sum;
+    }
 }
