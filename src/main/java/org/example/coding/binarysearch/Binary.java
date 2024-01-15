282390f62061be03f3be614fa3d3a242d2b84e53
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-01-15 22:27:52 +0530

    jan-15

--- src/main/java/org/example/coding/binarysearch/Binary.java
+++ src/main/java/org/example/coding/binarysearch/Binary.java
@@ -183,4 +183,75 @@ public class Binary {
 
         System.out.println(floorElement+" "+ceilingElement);
     }
+    public int findFirst(int nums[],int target){
+        int low=0,high=nums.length-1;
+        int index=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                index=mid;
+                high=mid-1;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return index;
+    }
+
+    public int findLast(int nums[],int target){
+        int low=0,high=nums.length-1;
+        int index=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                index=mid;
+                low=mid+1;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return index;
+    }
+
+    /*
+    * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
+    *
+    *
+    * 34. Find First and Last Position of Element in Sorted Array
+    *
+    * Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+    *
+    * */
+    public int[] searchRange(int[] nums, int target) {
+        int first=findFirst(nums,target);
+        int last=findLast(nums,target);
+        return new int[]{first,last};
+    }
+
+    public int search(int[] nums, int target) {
+        int low=0,high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>target){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(nums[mid]<target && nums[high]>=target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
 }
