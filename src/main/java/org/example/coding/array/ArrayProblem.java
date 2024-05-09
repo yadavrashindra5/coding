a73a4bf35937333ba502bf3f98390fb9953aaa87
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-05-10 02:59:59 +0530

    may-10

--- src/main/java/org/example/coding/array/ArrayProblem.java
+++ src/main/java/org/example/coding/array/ArrayProblem.java
@@ -1,6 +1,8 @@
 package org.example.coding.array;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ArrayProblem {
 
@@ -69,11 +71,10 @@ public class ArrayProblem {
     }
 
     /**
-     *Remove Duplicates from Sorted Array
-     *
+     * Remove Duplicates from Sorted Array
+     * <p>
      * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
-     *
-     * */
+     */
 
     public int removeDuplicates(int nums[]) {
         int i = 0, j = 0;
@@ -88,31 +89,26 @@ public class ArrayProblem {
     }
 
     /**
-     *
      * Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
-     *
+     * <p>
      * https://leetcode.com/problems/rotate-array/
-     *
-     * */
+     */
     public void rotate(int[] nums, int k) {
-        int index=nums.length-k;
-        nums=reverseArray(nums,0,index-1);
-        nums=reverseArray(nums,index,nums.length-1);
-        nums=reverseArray(nums,0,nums.length-1);
+        int index = nums.length - k;
+        nums = reverseArray(nums, 0, index - 1);
+        nums = reverseArray(nums, index, nums.length - 1);
+        nums = reverseArray(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
     }
 
     /**
      * reverse array
-     *
-     *
-     *
-     * */
-    public int [] reverseArray(int nums[],int startIndex,int endIndex){
-        while(startIndex<endIndex){
-            int temp=nums[startIndex];
-            nums[startIndex]=nums[endIndex];
-            nums[endIndex]=temp;
+     */
+    public int[] reverseArray(int nums[], int startIndex, int endIndex) {
+        while (startIndex < endIndex) {
+            int temp = nums[startIndex];
+            nums[startIndex] = nums[endIndex];
+            nums[endIndex] = temp;
             startIndex++;
             endIndex--;
         }
@@ -120,23 +116,23 @@ public class ArrayProblem {
     }
 
     /*
-    * Move Zeroes
-    *
-    * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-    *
-    * https://leetcode.com/problems/move-zeroes/description/
-    *
-    *
-    * */
+     * Move Zeroes
+     *
+     * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+     *
+     * https://leetcode.com/problems/move-zeroes/description/
+     *
+     *
+     * */
     public void moveZeroes(int[] nums) {
-        int i=0,j=0;
-        while(j<nums.length){
-            if(nums[i]!=0&&nums[j]!=0){
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            if (nums[i] != 0 && nums[j] != 0) {
                 i++;
-            }else if(nums[i]==0&&nums[j]!=0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
+            } else if (nums[i] == 0 && nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
                 i++;
             }
             j++;
@@ -144,74 +140,69 @@ public class ArrayProblem {
     }
 
     /*
-    *
-    * Missing Number
-    *
-    *
-    *Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
-    *
-    *
-    * */
+     *
+     * Missing Number
+     *
+     *
+     *Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+     *
+     *
+     * */
     public int missingNumber(int[] nums) {
-        int sum=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            sum+=nums[i];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
         }
-        int sum1=nums.length*(nums.length+1)/2;
-        return sum1-sum;
+        int sum1 = nums.length * (nums.length + 1) / 2;
+        return sum1 - sum;
     }
 
     /**
-     *
      * https://leetcode.com/problems/max-consecutive-ones/
      * Given a binary array nums, return the maximum number of consecutive 1's in the array.
      * 485. Max Consecutive Ones
-     * */
+     */
     public int findMaxConsecutiveOnes(int[] nums) {
-        int numsSize=nums.length;
-        int lengthOfOnes=0;
-        int count=0;
-        for(int i=0;i<numsSize;++i){
-            if(nums[i]==0){
-                lengthOfOnes=lengthOfOnes<count?count:lengthOfOnes;
-                count=0;
-            }else{
+        int numsSize = nums.length;
+        int lengthOfOnes = 0;
+        int count = 0;
+        for (int i = 0; i < numsSize; ++i) {
+            if (nums[i] == 0) {
+                lengthOfOnes = lengthOfOnes < count ? count : lengthOfOnes;
+                count = 0;
+            } else {
                 count++;
             }
         }
-        lengthOfOnes=lengthOfOnes<count?count:lengthOfOnes;
+        lengthOfOnes = lengthOfOnes < count ? count : lengthOfOnes;
         return lengthOfOnes;
     }
 
 
     /**
-     *
      * https://leetcode.com/problems/single-number/
-     *
+     * <p>
      * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
-     *
+     * <p>
      * 136. Single Number
-     * */
+     */
     public int singleNumber(int[] nums) {
-        int sum=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            sum=sum^nums[i];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum ^ nums[i];
         }
         return sum;
     }
 
-    public  int longestSubarrayWithSumK(int []a, long k) {
-        int sum=0,longSub=0;
-        for(int i=0;i<a.length;++i){
-            for(int j=i;j<a.length;++j){
-                sum+=a[j];
-                if(sum==k){
-                    longSub=longSub>(j-i+1)?longSub:(j-i+1);
-                }
-                else if(sum>k){
-                    sum=0;
+    public int longestSubarrayWithSumK(int[] a, long k) {
+        int sum = 0, longSub = 0;
+        for (int i = 0; i < a.length; ++i) {
+            for (int j = i; j < a.length; ++j) {
+                sum += a[j];
+                if (sum == k) {
+                    longSub = longSub > (j - i + 1) ? longSub : (j - i + 1);
+                } else if (sum > k) {
+                    sum = 0;
                     break;
                 }
             }
@@ -220,23 +211,23 @@ public class ArrayProblem {
     }
 
     public boolean findRotation(int[][] mat, int[][] target) {
-        int sizeOfMat=mat.length;
-        for(int i=0;i<4;++i){
-            boolean flag=true;
-            mat=transposeMatrix(mat);
-            flag=compareElement(mat,target,flag);
-            if(flag){
+        int sizeOfMat = mat.length;
+        for (int i = 0; i < 4; ++i) {
+            boolean flag = true;
+            mat = transposeMatrix(mat);
+            flag = compareElement(mat, target, flag);
+            if (flag) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean compareElement(int [][]mat,int target[][],boolean flag){
-        int sizeOfMatrix=mat.length;
-        for(int i=0;i<sizeOfMatrix;++i){
-            for(int j=0;j<sizeOfMatrix;++j){
-                if(mat[i][j]!=target[i][j]){
+    public boolean compareElement(int[][] mat, int target[][], boolean flag) {
+        int sizeOfMatrix = mat.length;
+        for (int i = 0; i < sizeOfMatrix; ++i) {
+            for (int j = 0; j < sizeOfMatrix; ++j) {
+                if (mat[i][j] != target[i][j]) {
                     return false;
                 }
             }
@@ -244,24 +235,45 @@ public class ArrayProblem {
         return flag;
     }
 
-    public int[][] transposeMatrix(int [][]mat){
-        int sizeOfMatrix=mat.length;
-        int newArray[][]=new int[sizeOfMatrix][sizeOfMatrix];
-        for(int i=0;i<sizeOfMatrix;++i){
-            for(int j=0;j<sizeOfMatrix;++j){
-                newArray[i][j]=mat[sizeOfMatrix+j-1][i];
+    public int[][] transposeMatrix(int[][] mat) {
+        int sizeOfMatrix = mat.length;
+        int newArray[][] = new int[sizeOfMatrix][sizeOfMatrix];
+        for (int i = 0; i < sizeOfMatrix; ++i) {
+            for (int j = 0; j < sizeOfMatrix; ++j) {
+                newArray[i][j] = mat[sizeOfMatrix + j - 1][i];
             }
         }
         printResult(newArray);
         return newArray;
     }
 
-    public void printResult(int [][]mat){
-        int sizeOfMatrix=mat.length;
-        for(int i=0;i<sizeOfMatrix;++i){
-            for(int j=0;j<sizeOfMatrix;++j){
+    public void printResult(int[][] mat) {
+        int sizeOfMatrix = mat.length;
+        for (int i = 0; i < sizeOfMatrix; ++i) {
+            for (int j = 0; j < sizeOfMatrix; ++j) {
                 System.out.println(mat[i][j]);
             }
         }
+    }
+
+    public long maximumHappinessSum(int[] happiness, int k) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < happiness.length; i++) {
+            list.add(happiness[i]);
+        }
+
+        Collections.sort(list);
+
+        long result = 0;
+        int i = 0;
+        while (i < k) {
+            if ((list.getLast() - i) > 0) {
+                result += list.removeLast() - i;
+                i++;
+            } else {
+                break;
+            }
+        }
+        return result;
     }
 }
---
+++ src/main/java/org/example/coding/array/Main.java
@@ -0,0 +1,15 @@
+package org.example.coding.array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayProblem arrayProblem=new ArrayProblem();
+        int arr[]={2,3,4,5};
+//        System.out.println(arrayProblem.maximumHappinessSum(arr,2));
+        System.out.println(true&&false);
+    }
+}
