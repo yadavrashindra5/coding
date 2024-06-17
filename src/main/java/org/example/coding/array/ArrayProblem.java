21d2d168ed634d135e8192ac62dbba6938fc1da1
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-06-18 01:20:23 +0530

    18 june 2024

--- src/main/java/org/example/coding/array/ArrayProblem.java
+++ src/main/java/org/example/coding/array/ArrayProblem.java
@@ -379,4 +379,33 @@ public class ArrayProblem {
 
         return true;
     }
+    /*
+    * 974. Subarray Sums Divisible by K
+    *
+    * Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.A subarray is a contiguous part of an array.
+    *
+    *  Example 1:
+    * Input: nums = [4,5,0,-2,-3,1], k = 5
+    * Output: 7
+    * Explanation: There are 7 subarrays with a sum divisible by k = 5:
+    * [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+    *
+    * */
+    public int subarraysDivByK(int[] nums, int k) {
+
+        int i=0,j=nums.length-1,result=0,sum=0;
+        while (i<=j){
+            if(i==j){
+                sum+=nums[i];
+                if(sum%k==0){
+                    result+=1;
+                }
+            }else if(sum<k){
+                sum+=nums[i];
+                j++;
+            }
+        }
+
+        return 0;
+    }
 }
