6e7828b7be44210f4a2c2440005169861624c9bc
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-06-20 00:29:52 +0530

    Jun 20 2024

--- src/main/java/org/example/coding/slidingwindow/Main.java
+++ src/main/java/org/example/coding/slidingwindow/Main.java
@@ -2,8 +2,11 @@ package org.example.coding.slidingwindow;
 
 public class Main {
     public static void main(String[] args) {
-        SlidingWindowProblem s=new SlidingWindowProblem();
+        SlidingWindowProblem s = new SlidingWindowProblem();
 //        s.maximumSum(new int[]{1,5,4,2,9,9,9},5);
-        s.countGoodSubstrings("xyzzak");
+//        s.countGoodSubstrings("xyzzak");
+        int arr[]={1,2,3,4};
+        int k=0;
+        s.decrypt(arr,k);
     }
 }
--- src/main/java/org/example/coding/slidingwindow/SlidingWindowProblem.java
+++ src/main/java/org/example/coding/slidingwindow/SlidingWindowProblem.java
@@ -1,19 +1,20 @@
 package org.example.coding.slidingwindow;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SlidingWindowProblem {
-    public void maximumSum(int arr[], int k){
-        int i=0,j=0,sum=0,sizeOfArr=arr.length,max=Integer.MIN_VALUE;
-        while (j<sizeOfArr){
-            sum+=arr[j];
-            if((j-i+1)<k){
+    public void maximumSum(int arr[], int k) {
+        int i = 0, j = 0, sum = 0, sizeOfArr = arr.length, max = Integer.MIN_VALUE;
+        while (j < sizeOfArr) {
+            sum += arr[j];
+            if ((j - i + 1) < k) {
                 j++;
-            }else{
-                max=Math.max(max,sum);
-                sum=sum-arr[i];
+            } else {
+                max = Math.max(max, sum);
+                sum = sum - arr[i];
                 j++;
                 i++;
             }
@@ -21,23 +22,56 @@ public class SlidingWindowProblem {
         System.out.println(max);
     }
 
+    public int[] decrypt(int[] code, int k) {
+
+        int result[] = new int[code.length];
+
+        int size = code.length;
+        for (int i = 0; i < code.length; ++i) {
+            if (k > 0) {
+                int x = (i + 1) % size;
+                int c = 0;
+                int sum = 0;
+                while (c < Math.abs(k)) {
+                    sum += code[x];
+                    x = (x + 1) % size;
+                    c++;
+                }
+                result[i]=sum;
+            } else if (k < 0) {
+                int x = Math.abs((i+size - 1)) % size;
+                int c = 0;
+                int sum = 0;
+                while (c < Math.abs(k)) {
+                    sum += code[x];
+                    x = Math.abs((x - 1)) % size;
+                    c++;
+                }
+                result[i]=sum;
+            } else {
+                result[i]=0;
+            }
+        }
+        return result;
+    }
+
     public int countGoodSubstrings(String s) {
-        HashMap<Character,Integer>map=new HashMap<>();
-        int i=0,j=0,size=s.length(),count=0;
-        while (j<size){
-            Character currentChar=s.charAt(j);
-            map.put(currentChar,map.getOrDefault(currentChar,0)+1);
-            if(j-i+1<3){
+        HashMap<Character, Integer> map = new HashMap<>();
+        int i = 0, j = 0, size = s.length(), count = 0;
+        while (j < size) {
+            Character currentChar = s.charAt(j);
+            map.put(currentChar, map.getOrDefault(currentChar, 0) + 1);
+            if (j - i + 1 < 3) {
                 j++;
-            }else {
-                if(map.size()==3){
+            } else {
+                if (map.size() == 3) {
                     count++;
                 }
                 j++;
-                if(map.get(s.charAt(i))==1){
+                if (map.get(s.charAt(i)) == 1) {
                     map.remove(s.charAt(i));
-                }else{
-                    map.put(s.charAt(i),map.get(s.charAt(i))-1);
+                } else {
+                    map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
                 }
                 i++;
             }
