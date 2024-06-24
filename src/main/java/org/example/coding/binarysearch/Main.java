43a045df4e5ecd321a45c34596dac1495f5ec13f
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-06-24 20:31:36 +0530

    June-24-2024

---
+++ src/main/java/org/example/coding/binarysearch/Main.java
@@ -0,0 +1,8 @@
+package org.example.coding.binarysearch;
+
+public class Main {
+    public static void main(String[] args) {
+        Binary binary = new Binary();
+        int arr[] = {1, 2, 2, 2, 3, 5, 6};
+    }
+}
