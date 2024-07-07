75c11f0c108df7a8440001790f1a0c6258a83b09
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-07-07 23:03:02 +0530

    7-July-2024

--- src/main/java/org/example/coding/dailyquestion/Problems.java
+++ src/main/java/org/example/coding/dailyquestion/Problems.java
@@ -48,4 +48,14 @@ public class Problems {
         }
         return result;
     }
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int totalBottles = numBottles;
+
+        while (numBottles >= numExchange) {
+            totalBottles += numBottles / numExchange;
+            numBottles = (numBottles / numExchange) + (numBottles % numExchange);
+        }
+
+        return totalBottles;
+    }
 }
