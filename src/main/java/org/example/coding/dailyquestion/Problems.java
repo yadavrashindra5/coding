b1120a079dc327b6eaeeffd613d94ee634dacf4f
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-07-07 23:01:29 +0530

    7-July-2024

--- src/main/java/org/example/coding/dailyquestion/Problems.java
+++ src/main/java/org/example/coding/dailyquestion/Problems.java
@@ -1,61 +1,51 @@
 package org.example.coding.dailyquestion;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Problems {
     public boolean threeConsecutiveOdds(int[] arr) {
-        int k=3,i=0,j=0;
-        while (j<arr.length){
-            if((j-i+1)<k){
+        int k = 3, i = 0, j = 0;
+        while (j < arr.length) {
+            if ((j - i + 1) < k) {
                 j++;
-            }else{
-                boolean flag=true;
-                for(int x=i;x<=j;++x){
-                    if(arr[x]%2==0){
-                        flag=false;
+            } else {
+                boolean flag = true;
+                for (int x = i; x <= j; ++x) {
+                    if (arr[x] % 2 == 0) {
+                        flag = false;
                         break;
                     }
                 }
-                if(flag){
-                   return true;
+                if (flag) {
+                    return true;
                 }
                 i++;
                 j++;
             }
         }
-        return  false;
+        return false;
     }
 
 
     public int[] intersect(int[] nums1, int[] nums2) {
-        //always consider num1 is greater
-        if(nums1.length<nums2.length){
-            int temp[]=nums1;
-            nums1=nums2;
-            nums2=temp;
+        Map<Integer, Integer> map1 = new TreeMap<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i : nums1) {
+            map1.put(i, map1.getOrDefault(i, 0) + 1);
         }
 
-        int sizeOfNums1=nums1.length;
-        int sizeOfNums2=nums2.length;
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-
-        List<Integer>list=new ArrayList<>();
-        int i=0,j=0;
-        while (i<sizeOfNums1&&j<sizeOfNums2){
-            if(nums1[i]==nums2[j]){
-                list.add(nums1[i]);
-                i++;
-                j++;
-            }else if(nums1[i]!=nums2[j] && nums1[i]<nums2[j]){
-                i++;
-            }else if(nums1[i]!=nums2[j] && nums2[j]<nums1[i]){
-                j++;
+        for (int i = 0; i < nums2.length; ++i) {
+            int currentIndexValue=nums2[i];
+            if (map1.containsKey(currentIndexValue) && map1.get(currentIndexValue) != 0) {
+                list.add(currentIndexValue);
+                map1.put(currentIndexValue, map1.get(currentIndexValue) - 1);
             }
         }
-
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        int result[]=new int[list.size()];
+        for(int i=0;i<list.size();++i){
+            result[i]=list.get(i);
+        }
+        return result;
     }
 }
--- src/main/java/org/example/coding/prefixsum/Prefix.java
+++ src/main/java/org/example/coding/prefixsum/Prefix.java
@@ -3,7 +3,7 @@ package org.example.coding.prefixsum;
 import java.util.*;
 import java.util.stream.IntStream;
 
-public class Prefix{
+public class Prefix {
     public int[] runningSum(int[] nums) {
 //        List<Integer>list=new ArrayList<>();
 //        int sum=0;
@@ -12,95 +12,117 @@ public class Prefix{
 //        }
 //        return list.stream().mapToInt(Integer::intValue).toArray();
 
-        int result[]=new int[nums.length];
+        int result[] = new int[nums.length];
         int sum = 0;
         for (int i = 0; i < nums.length; ++i) {
-            sum=sum+nums[i];
-            result[i]=sum;
+            sum = sum + nums[i];
+            result[i] = sum;
         }
         return result;
     }
 
-    public int modifyBinarySearch(int prefixSum[],int target){
-        int sizeOfPrefix=prefixSum.length;
-        int startIndex=0,endIndex=sizeOfPrefix-1,index=-1;
-        while (startIndex<=endIndex){
-            int mid=(startIndex+endIndex)/2;
-            if(prefixSum[mid]==target){
-                index=mid;
-                return index+1;
-            }else if(prefixSum[mid]<target){
-                index=mid;
-                startIndex=mid+1;
-            }else{
-                endIndex=mid-1;
+    public int modifyBinarySearch(int prefixSum[], int target) {
+        int sizeOfPrefix = prefixSum.length;
+        int startIndex = 0, endIndex = sizeOfPrefix - 1, index = -1;
+        while (startIndex <= endIndex) {
+            int mid = (startIndex + endIndex) / 2;
+            if (prefixSum[mid] == target) {
+                index = mid;
+                return index + 1;
+            } else if (prefixSum[mid] < target) {
+                index = mid;
+                startIndex = mid + 1;
+            } else {
+                endIndex = mid - 1;
             }
         }
-        return index+1;
+        return index + 1;
     }
 
     public int[] answerQueries(int[] nums, int[] queries) {
-       int []prefixSum=new int[nums.length];
-       int []ans=new int[queries.length];
+        int[] prefixSum = new int[nums.length];
+        int[] ans = new int[queries.length];
 
-       Arrays.sort(nums);
+        Arrays.sort(nums);
 
-       TreeMap<Integer,Integer>map=new TreeMap<>();
-       prefixSum[0]=nums[0];
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        prefixSum[0] = nums[0];
 
-       map.put(prefixSum[0],1);
+        map.put(prefixSum[0], 1);
 
-       for(int i=0;i<nums.length;++i){
-           prefixSum[i]=prefixSum[i-1]+nums[i];
-           map.put(prefixSum[i],i+1);
-       }
+        for (int i = 0; i < nums.length; ++i) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+            map.put(prefixSum[i], i + 1);
+        }
 
-       int k=0;
-       for(int query:queries){
-           Map.Entry<Integer,Integer>entry=map.floorEntry(query);
-           if(entry!=null){
-               ans[k]=entry.getValue();
-           }
-           k++;
-       }
+        int k = 0;
+        for (int query : queries) {
+            Map.Entry<Integer, Integer> entry = map.floorEntry(query);
+            if (entry != null) {
+                ans[k] = entry.getValue();
+            }
+            k++;
+        }
         return ans;
     }
 
     public int findMiddleIndex(int[] nums) {
-        int leftSum=0,rightSum=0;
+        int leftSum = 0, rightSum = 0;
 
-        for(int i:nums){
-            rightSum+=i;
+        for (int i : nums) {
+            rightSum += i;
         }
 
-        int i=0,size=nums.length;
+        int i = 0, size = nums.length;
 
-        while (i<size){
-           rightSum=rightSum-nums[i];
-           if(leftSum==rightSum){
-               return i;
-           }
-           leftSum+=nums[i];
-           i++;
+        while (i < size) {
+            rightSum = rightSum - nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+            i++;
         }
         return -1;
     }
 
     public int pivotIndex(int[] nums) {
-        int leftSum=0,rightSum=0;
-        for(int i:nums){
-            rightSum+=i;
+        int leftSum = 0, rightSum = 0;
+        for (int i : nums) {
+            rightSum += i;
         }
 
-        int i=0,size=nums.length;
-        while (i<size){
-            rightSum-=nums[i];
-            if(leftSum==rightSum){
+        int i = 0, size = nums.length;
+        while (i < size) {
+            rightSum -= nums[i];
+            if (leftSum == rightSum) {
                 return i;
             }
-            leftSum+=nums[i];
+            leftSum += nums[i];
             i++;
         }
         return -1;
+    }
+
+    public int maximumPopulation(int[][] logs) {
+        int row = logs.length;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < row; ++i) {
+            int person[] = logs[i];
+            int birth = person[0];
+            int death = person[1];
+            int count = 0;
+            for (int j = 0; j < row; ++j) {
+                int birthx = logs[j][0];
+                if (birthx >= birth && birthx <= death) {
+                    count++;
+                }
+            }
+            if(count>max){
+                max=count;
+            }
+        }
+        System.out.println(max);
+        return 1;
     }
 }
