c94f6a0411f8cd945ff808a925c08a7884993ab4
Rashindra Yadav <yadavrashindra5@gmail.com>
2024-07-23 00:08:51 +0530

    23-July-2024

--- src/main/java/org/example/coding/twopointer/Problems.java
+++ src/main/java/org/example/coding/twopointer/Problems.java
@@ -1,9 +1,6 @@
 package org.example.coding.twopointer;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Problems {
@@ -22,6 +19,45 @@ public class Problems {
             }
         }
         return count;
+    }
+
+    public int[] sortArrayByParity(int[] nums) {
+        int n = nums.length;
+        int i = 0, j = n - 1;
+        while (i < j) {
+            //if i=odd and j=odd
+//            {3, 1, 2, 4}
+            if (nums[i] % 2 == 0) {
+                i++;
+            } else if (nums[j] % 2 == 1) {
+                j--;
+            } else {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        return nums;
+    }
+
+    public int[] sortArrayByParityII(int[] nums) {
+        int n = nums.length, i = 0, j = 1, k = 0;
+        while (j < n && i < n) {
+            if (nums[i] % 2 == 0) {
+                i += 2;
+            } else if (nums[j] % 2 != 0) {
+                j += 2;
+            } else {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i += 2;
+                j += 2;
+            }
+        }
+        return nums;
     }
 
 }
