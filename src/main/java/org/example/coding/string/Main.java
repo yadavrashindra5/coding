1ab1672c75f6dbfb716ee6b4a6cedef2cda5c034
Rashindra Yadav <yadavrashindra5@gmail.com>
2025-02-25 00:20:45 +0530

    25-Feb-2025

--- src/main/java/org/example/coding/string/Main.java
+++ src/main/java/org/example/coding/string/Main.java
@@ -5,35 +5,9 @@ import java.util.Map.Entry;
 
 public class Main {
     public static void main(String[] args) {
-
-        int score[]={100,11,111111};
-        Map<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<score.length;++i){
-            map.put(score[i],i);
-        }
-
-        Set<Integer>set=map.keySet();
-        List<Integer>list=new ArrayList<>(set);
-        System.out.println(list);
-        Collections.sort(list);
-        System.out.println(list);
-        Collections.reverse(list);
-        System.out.println(list);
-
-        String result[]=new String[map.size()];
-        for(int i=0;i<list.size();++i){
-            int value=list.get(i);
-            int index=map.get(value);
-            if(i==0){
-                result[index]="Gold Medal";
-            }else if(i==1){
-                result[index]="Silver Medal";
-            }else if(i==2){
-                result[index]="Bronze Medal";
-            }else{
-                result[index]=String.valueOf(i+1);
-            }
-        }
+        StringProblem stringProblem = new StringProblem();
+        String str = "001011";
+        int result[] = stringProblem.minOperations(str);
         System.out.println(Arrays.toString(result));
     }
 }
--- src/main/java/org/example/coding/string/StringProblem.java
+++ src/main/java/org/example/coding/string/StringProblem.java
@@ -5,25 +5,25 @@ import java.util.Arrays;
 public class StringProblem {
 
     /*
-    *https://leetcode.com/problems/remove-outermost-parentheses/
-    *1021. Remove Outermost Parentheses
-    *
-    * A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
-    *
-    * */
+     *https://leetcode.com/problems/remove-outermost-parentheses/
+     *1021. Remove Outermost Parentheses
+     *
+     * A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+     *
+     * */
     public String removeOuterParentheses(String s) {
-        int count=0;
-        int sizeOfS=s.length();
-        String result="";
-        for(int i=0;i<sizeOfS;++i){
-            if(s.charAt(i)=='('){
-                if(count>=1){
-                    result+=s.charAt(i);
+        int count = 0;
+        int sizeOfS = s.length();
+        String result = "";
+        for (int i = 0; i < sizeOfS; ++i) {
+            if (s.charAt(i) == '(') {
+                if (count >= 1) {
+                    result += s.charAt(i);
                 }
                 count++;
-            }else{
-                if(count>1){
-                    result+=s.charAt(i);
+            } else {
+                if (count > 1) {
+                    result += s.charAt(i);
                 }
                 count--;
             }
@@ -33,71 +33,116 @@ public class StringProblem {
 
 
     /*
-    *
-    * https://leetcode.com/problems/reverse-words-in-a-string/
-    *
-    * 151. Reverse Words in a String
-    *
-    * Given an input string s, reverse the order of the words.
-    * A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
-    *
-    * */
+     *
+     * https://leetcode.com/problems/reverse-words-in-a-string/
+     *
+     * 151. Reverse Words in a String
+     *
+     * Given an input string s, reverse the order of the words.
+     * A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+     *
+     * */
     public String reverseWords(String s) {
         String trim = s.trim();
-        String splitString[]=trim.split(" ");
-        int splitSize=splitString.length-1;
-        String result="";
-        for(int i=splitSize;i>=0;--i){
-            if(!splitString[i].isEmpty()){
-                result+=splitString[i].trim()+" ";
+        String splitString[] = trim.split(" ");
+        int splitSize = splitString.length - 1;
+        String result = "";
+        for (int i = splitSize; i >= 0; --i) {
+            if (!splitString[i].isEmpty()) {
+                result += splitString[i].trim() + " ";
             }
         }
         return result.trim();
     }
 
     /*
-    * https://leetcode.com/problems/largest-odd-number-in-string/
-    *
-    * 1903. Largest Odd Number in String
-    *
-    * */
+     * https://leetcode.com/problems/largest-odd-number-in-string/
+     *
+     * 1903. Largest Odd Number in String
+     *
+     * */
     public String largestOddNumber(String str) {
-        int sizeOfString=str.length()-1;
-        for(int i=sizeOfString;i>=0;--i){
-            char lastCharacter=str.charAt(i);
-            int isOddOrEven=Character.getNumericValue(lastCharacter);
-            if(isOddOrEven%2!=0){
-                return str.substring(0,i+1);
+        int sizeOfString = str.length() - 1;
+        for (int i = sizeOfString; i >= 0; --i) {
+            char lastCharacter = str.charAt(i);
+            int isOddOrEven = Character.getNumericValue(lastCharacter);
+            if (isOddOrEven % 2 != 0) {
+                return str.substring(0, i + 1);
             }
         }
         return "";
     }
 
     public int compareVersion(String version1, String version2) {
-        String v1[]=version1.split("\\.");
-        String v2[]=version2.split("\\.");
-        int lengthOfV1=v1.length,lengthOfV2=v2.length,i=0,j=0;
-        while (i<lengthOfV1&&j<lengthOfV2){
-            if(Integer.valueOf(v1[i])<Integer.valueOf(v2[i])){
+        String v1[] = version1.split("\\.");
+        String v2[] = version2.split("\\.");
+        int lengthOfV1 = v1.length, lengthOfV2 = v2.length, i = 0, j = 0;
+        while (i < lengthOfV1 && j < lengthOfV2) {
+            if (Integer.valueOf(v1[i]) < Integer.valueOf(v2[i])) {
                 return -1;
-            }else if(Integer.valueOf(v1[i])>Integer.valueOf(v2[i])){
+            } else if (Integer.valueOf(v1[i]) > Integer.valueOf(v2[i])) {
                 return 1;
             }
             i++;
             j++;
         }
-        while (i<lengthOfV1){
-            if(Integer.valueOf(v1[i])>0){
+        while (i < lengthOfV1) {
+            if (Integer.valueOf(v1[i]) > 0) {
                 return 1;
             }
             i++;
         }
-        while (j<lengthOfV2){
-            if(Integer.valueOf(v2[j])>0){
+        while (j < lengthOfV2) {
+            if (Integer.valueOf(v2[j]) > 0) {
                 return -1;
             }
             j++;
         }
         return 0;
+    }
+
+
+    public int scoreOfString(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length() - 1; ++i) {
+            int absDifference = Math.abs((s.charAt(i) - s.charAt(i + 1)));
+            sum += absDifference;
+        }
+        return sum;
+    }
+
+    /*
+    * https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box
+    * solve this problem using cummulative approach
+    * */
+
+    public int[] minOperations(String boxes) {
+        int leftCummulative[] = new int[boxes.length()];
+        int rightCummulative[] = new int[boxes.length()];
+
+        int left = 0, right = 0;
+        for (int i = 0; i < boxes.length(); ++i) {
+            left += Character.getNumericValue(boxes.charAt(i));
+            leftCummulative[i] = left;
+        }
+
+        for (int i = boxes.length() - 1; i >= 0; --i) {
+            right += Character.getNumericValue(boxes.charAt(i));
+            rightCummulative[i] = right;
+        }
+
+        int prefixsum = 0, suffix = 0;
+        for (int i = 0; i < rightCummulative.length; ++i) {
+            suffix += rightCummulative[i];
+        }
+
+        int result[] = new int[boxes.length()];
+
+        for (int i = 0; i < boxes.length(); ++i) {
+            result[i] = prefixsum + suffix - rightCummulative[i];
+            prefixsum += leftCummulative[i];
+            suffix -= rightCummulative[i];
+        }
+        return result;
     }
 }
